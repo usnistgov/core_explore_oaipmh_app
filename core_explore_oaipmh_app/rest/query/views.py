f98009dcbd848b30ac68668bd4cdf0c78f4ce3b6
Pierre Francois RIGODIAT <pierre-francois.rigodiat@nist.gov>
2017-06-29 12:49:47 -0400

    Handle pagination in REST API
    Change-Id: I10f2efa5b6dfabfd412b92b0d35d5da44b8b208c

--- core_explore_oaipmh_app/rest/query/views.py
+++ core_explore_oaipmh_app/rest/query/views.py
@@ -1,18 +1,23 @@
 """ REST views for the explore OAI-PMH  API
 """
+import json
+
+import core_oaipmh_harvester_app.components.oai_record.api as oai_record_api
+from core_main_app.utils.xml import unparse
 from core_oaipmh_common_app.commons.messages import OaiPmhMessage
+from core_oaipmh_harvester_app.components.oai_harvester_metadata_format import api \
+    as oai_harvester_metadata_format_api
 from django.core.urlresolvers import reverse
-from core_explore_common_app.components.result.models import Result
-from core_explore_common_app.rest.result.serializers import ResultSerializer
-from core_explore_oaipmh_app.utils.query.mongo.query_builder import OaiPmhQueryBuilder
-from core_oaipmh_harvester_app.components.oai_harvester_metadata_format import api as oai_harvester_metadata_format_api
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import status
-from core_main_app.utils.xml import unparse
+
+from core_explore_common_app.components.result.models import Result
+from core_explore_common_app.rest.result.serializers import ResultSerializer
+from core_explore_common_app.utils.pagination.rest_framework_paginator.pagination \
+    import StandardResultsSetPagination
 from core_explore_common_app.utils.result import result as result_utils
-import core_oaipmh_harvester_app.components.oai_record.api as oai_record_api
-import json
+from core_explore_oaipmh_app.utils.query.mongo.query_builder import OaiPmhQueryBuilder
 
 
 @api_view(['POST'])
@@ -34,7 +39,8 @@ def execute_query(request):
         if query is not None:
             query_builder = OaiPmhQueryBuilder(query, 'metadata')
         else:
-            return Response('Query should be passed in parameter', status=status.HTTP_400_BAD_REQUEST)
+            return Response('Query should be passed in parameter',
+                            status=status.HTTP_400_BAD_REQUEST)
 
         if options is not None:
             json_options = json.loads(options)
@@ -49,23 +55,30 @@ def execute_query(request):
                 # get list of template ids
                 list_template_ids = [template['id'] for template in templates]
                 # get all metadata formats used by the instance (registry)
-                list_metadata_format = oai_harvester_metadata_format_api.get_all_by_registry_id(instance_id)
+                list_metadata_format = oai_harvester_metadata_format_api.\
+                    get_all_by_registry_id(instance_id)
                 # Filter metadata formats that use the given templates
                 list_metadata_formats_id = [str(x.id) for x in list_metadata_format
-                                            if x.template is not None and str(x.template.id) in list_template_ids]
+                                            if x.template is not None
+                                            and str(x.template.id) in list_template_ids]
                 query_builder.add_list_metadata_formats_criteria(list_metadata_formats_id)
 
         # create a raw query
         raw_query = query_builder.get_raw_query()
         # execute query
         data_list = oai_record_api.execute_query(raw_query)
+        # get paginator
+        paginator = StandardResultsSetPagination()
+        # get requested page from list of results
+        page = paginator.paginate_queryset(data_list, request)
+
         # Serialize object
         results = []
         url = reverse("core_explore_oaipmh_app_data_detail")
         url_access_data = reverse("core_explore_oaipmh_app_rest_get_result_from_data_id")
         # Template info
         template_info = dict()
-        for data in data_list:
+        for data in page:
             # get data's template
             template = data.harvester_metadata_format.template
             # get and store data's template information
@@ -76,12 +89,14 @@ def execute_query(request):
                                   xml_content=unparse(data.metadata),
                                   template_info=template_info[template],
                                   detail_url="{0}?id={1}".format(url, data.id),
-                                  access_data_url="{0}?id={1}".format(url_access_data, str(data.id))))
+                                  access_data_url="{0}?id={1}".format(url_access_data,
+                                                                      str(data.id))))
 
-        return_value = ResultSerializer(results, many=True)
-
-        return Response(return_value.data, status=status.HTTP_200_OK)
+        # Serialize results
+        serialized_results = ResultSerializer(results, many=True)
+        # Return http response
+        return paginator.get_paginated_response(serialized_results.data)
     except Exception as e:
-        content = OaiPmhMessage.get_message_labelled('An error occurred when attempting to execute the query: %s'
-                                                     % e.message)
+        content = OaiPmhMessage.get_message_labelled('An error occurred when attempting to execute'
+                                                     ' the query: %s' % e.message)
         return Response(content, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
--- core_explore_oaipmh_app/rest/urls.py
+++ core_explore_oaipmh_app/rest/urls.py
@@ -5,7 +5,7 @@ from core_explore_oaipmh_app.rest.query import views as query_views
 from core_explore_oaipmh_app.rest.result import views as result_views
 
 urlpatterns = [
-    url(r'^execute-query$', query_views.execute_query,
+    url(r'^execute-query', query_views.execute_query,
         name='core_explore_oaipmh_rest_execute_query'),
     url(r'^result', result_views.get_result_from_data_id,
         name='core_explore_oaipmh_app_rest_get_result_from_data_id')
