eb440c8373b8854abda6f04da0602f702cd6d0f1
Pierre Francois RIGODIAT <pierre-francois.rigodiat@nist.gov>
2018-04-04 13:59:25 -0400

    fix: Use AbstractExecuteQueryView
    Change-Id: Iff8bd68a17b10a54287216566002b96809fca17c

--- core_explore_oaipmh_app/rest/query/views.py
+++ core_explore_oaipmh_app/rest/query/views.py
@@ -1,75 +1,45 @@
-""" REST views for the explore OAI-PMH  API
+""" REST views for the explore OAI-PMH API
 """
 import json
 
 from django.core.urlresolvers import reverse
-from rest_framework import status
-from rest_framework.decorators import api_view, schema
-from rest_framework.response import Response
 
-import core_oaipmh_harvester_app.components.oai_record.api as oai_record_api
 from core_explore_common_app.components.result.models import Result
 from core_explore_common_app.rest.result.serializers import ResultSerializer
-from core_explore_oaipmh_app.utils.query.mongo.query_builder import OaiPmhQueryBuilder
 from core_main_app.utils.pagination.rest_framework_paginator.pagination import StandardResultsSetPagination
-from core_oaipmh_common_app.commons.messages import OaiPmhMessage
-from core_oaipmh_harvester_app.components.oai_harvester_metadata_format import api as oai_harvester_metadata_format_api
+from core_oaipmh_harvester_app.rest.oai_record.abstract_views import AbstractExecuteQueryView
 
 
-@api_view(['GET'])
-@schema(None)
-def execute_query(request):
-    """ Executes query and returns results.
+class ExecuteQueryView(AbstractExecuteQueryView):
+    def get_registries(self):
+        """ Get a list of registry ids. Should return empty list if not found. JSON format.
 
-    Args:
-        request: Request.
+        Returns:
+            List of registry ids (JSON format).
 
-    Returns:
-        Response: Response.
-
-    """
-    try:
-        # get query
-        query = request.data.get('query', None)
-        options = request.data.get('options', None)
-
-        if query is not None:
-            query_builder = OaiPmhQueryBuilder(query, 'dict_content')
-        else:
-            return Response('Query should be passed in parameter',
-                            status=status.HTTP_400_BAD_REQUEST)
-
+        """
+        registries = []
+        options = self.request.data.get('options', None)
         if options is not None:
             json_options = json.loads(options)
-            instance_id = json_options['instance_id']
-        else:
-            return Response('Missing instance information.', status=status.HTTP_400_BAD_REQUEST)
+            registries.append(json_options['instance_id'])
 
-        # update the content query with given templates
-        if 'templates' in request.data:
-            templates = json.loads(request.data['templates'])
-            if len(templates) > 0:
-                # get list of template ids
-                list_template_ids = [template['id'] for template in templates]
-                # get all metadata formats used by the instance (registry)
-                list_metadata_format = oai_harvester_metadata_format_api.\
-                    get_all_by_registry_id(instance_id)
-                # Filter metadata formats that use the given templates
-                list_metadata_formats_id = [str(x.id) for x in list_metadata_format
-                                            if x.template is not None
-                                            and str(x.template.id) in list_template_ids]
-                query_builder.add_list_metadata_formats_criteria(list_metadata_formats_id)
+        return json.dumps(registries)
 
-        # do not include deleted records
-        query_builder.add_not_deleted_criteria()
-        # create a raw query
-        raw_query = query_builder.get_raw_query()
-        # execute query
-        data_list = oai_record_api.execute_query(raw_query)
-        # get paginator
+    def build_response(self, data_list):
+        """ Build the paginated response.
+
+        Args:
+            data_list: List of data.
+
+        Returns:
+            The response.
+
+        """
+        # Paginator
         paginator = StandardResultsSetPagination()
         # get requested page from list of results
-        page = paginator.paginate_queryset(data_list, request)
+        page = paginator.paginate_queryset(data_list, self.request)
 
         # Serialize object
         results = []
@@ -83,7 +53,7 @@ def execute_query(request):
             # get and store data's template information from metadata format
             if metadata_format not in template_info:
                 template = data.harvester_metadata_format.template
-                template_info[metadata_format] =\
+                template_info[metadata_format] = \
                     get_template_info_from_metadata_format_and_template(metadata_format, template)
 
             results.append(Result(title=data.title,
@@ -97,10 +67,6 @@ def execute_query(request):
         serialized_results = ResultSerializer(results, many=True)
         # Return http response
         return paginator.get_paginated_response(serialized_results.data)
-    except Exception as e:
-        content = OaiPmhMessage.get_message_labelled('An error occurred when attempting to execute'
-                                                     ' the query: %s' % e.message)
-        return Response(content, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 def get_template_info_from_metadata_format_and_template(harvester_metadata_format, template):
