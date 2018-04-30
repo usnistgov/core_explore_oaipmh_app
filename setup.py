9f45b2157a84309508a97c7bc7d3b8667b328627
Adrien Catel <adrien.catel@nist.gov>
2018-04-30 13:17:23 -0400

    chore: update beta3
    Change-Id: Id87c83ebfcb25845ceadecaf10eacf40b3b5df00

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ chdir(normpath(join(abspath(__file__), pardir)))
 
 setup(
     name="core_explore_oaipmh_app",
-    version="1.0.0-beta2",
+    version="1.0.0-beta3",
     description="OAI-PMH exploration functions for curator core project",
     long_description=long_desc,
     author="NIST IT Lab",
