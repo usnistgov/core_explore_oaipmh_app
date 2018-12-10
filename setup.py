d0d648aeae62b2a779f20a1e26540393f4e40ebf
Adrien Catel <adrien.catel@nist.gov>
2018-12-10 11:39:26 -0500

    chore: upgrade version 1.0.0
    Change-Id: I3dad157bdcd4866ee41c6583f22568ea8efeef50

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ chdir(normpath(join(abspath(__file__), pardir)))
 
 setup(
     name="core_explore_oaipmh_app",
-    version="1.0.0-rc2",
+    version="1.0.0",
     description="OAI-PMH exploration functions for curator core project",
     long_description=long_desc,
     author="NIST IT Lab",
