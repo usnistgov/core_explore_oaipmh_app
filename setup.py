331e291b13cc5fc6b7c255ea0086d7329217bf8b
Xavier SCHMITT <xavier.schmitt@nist.gov>
2019-04-03 12:15:17 -0400

    chore: upgrade 1.2.0
    Change-Id: I4d576fffe400d1e385d8d3dffeb0e19d3200a235

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ chdir(normpath(join(abspath(__file__), pardir)))
 
 setup(
     name="core_explore_oaipmh_app",
-    version="1.1.0",
+    version="1.2.0",
     description="OAI-PMH exploration functions for curator core project",
     long_description=long_desc,
     author="NIST IT Lab",
