190c001587700e8b572dd477205f60daa79bfa73
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2019-10-30 10:16:07 -0400

    feat: upgrade django 2.2
    Change-Id: Ie388e3ff56c1fe0267bb4d321a9d35b87988ad48

--- core_explore_oaipmh_app/rest/urls.py
+++ core_explore_oaipmh_app/rest/urls.py
@@ -1,14 +1,15 @@
 """Url router for the REST API
 """
-from django.conf.urls import url
+
+from django.urls import re_path
 
 from core_explore_oaipmh_app.rest.query import views as query_views
 from core_explore_oaipmh_app.rest.result import views as result_views
 
 urlpatterns = [
-    url(r'^execute-query', query_views.ExecuteQueryView.as_view(),
-        name='core_explore_oaipmh_rest_execute_query'),
-    url(r'^result', result_views.get_result_from_data_id,
-        name='core_explore_oaipmh_app_rest_get_result_from_data_id')
+    re_path(r'^execute-query', query_views.ExecuteQueryView.as_view(),
+            name='core_explore_oaipmh_rest_execute_query'),
+    re_path(r'^result', result_views.get_result_from_data_id,
+            name='core_explore_oaipmh_app_rest_get_result_from_data_id')
 
 ]
