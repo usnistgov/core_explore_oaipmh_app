6d984948bdd7c0d6abed2d6bac42c8fa73bc4807
Philippe Dessauw <philippe.dessauw@nist.gov>
2020-09-02 16:40:33 -0400

    feat: use common html renderer for OAI data
    Change-Id: Iccd95500b12b2232635b027e892d24276e243055

--- core_explore_oaipmh_app/views/user/views.py
+++ core_explore_oaipmh_app/views/user/views.py
@@ -2,6 +2,7 @@
 """
 from core_main_app.utils.rendering import render
 from core_oaipmh_harvester_app.components.oai_record import api as oai_record_api
+from core_main_app.utils.view_builders import data as data_view_builder
 
 
 def data_detail(request):
@@ -13,14 +14,11 @@ def data_detail(request):
     Returns:
 
     """
-    record_id = request.GET["id"]
-    data = dict()
-
     try:
-        record = oai_record_api.get_by_id(record_id)
+        record = oai_record_api.get_by_id(request.GET["id"])
         template = record.harvester_metadata_format.template
 
-        data = {
+        data_object = {
             "title": record.identifier,
             "xml_content": record.xml_content,
             "template": {
@@ -29,6 +27,10 @@ def data_detail(request):
                 "hash": record.harvester_metadata_format.hash,
             },
         }
+
+        page_context = data_view_builder.build_page(data_object)
+
+        return data_view_builder.render_page(request, render, page_context)
     except Exception as e:
         error_message = "An error occured: {0}".format(str(e))
         return render(
@@ -36,17 +38,3 @@ def data_detail(request):
             "core_main_app/common/commons/error.html",
             context={"error": error_message},
         )
-
-    context = {"data": data}
-
-    assets = {
-        "js": [
-            {"path": "core_main_app/common/js/XMLTree.js", "is_raw": False},
-            {"path": "core_main_app/user/js/data/detail.js", "is_raw": False},
-        ],
-        "css": ["core_main_app/common/css/XMLTree.css"],
-    }
-
-    return render(
-        request, "core_main_app/user/data/detail.html", context=context, assets=assets
-    )
